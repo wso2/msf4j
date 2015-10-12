3f359bf93024293441624f6910820ed7a5fad55c
Afkham Azeez <azeez@wso2.com>
2015-10-12 12:26:19 +0530

    Added methods to pet microservice

--- samples/petstore/microservices/pet/src/main/java/org/wso2/carbon/mss/examples/petstore/pet/PetService.java
+++ samples/petstore/microservices/pet/src/main/java/org/wso2/carbon/mss/examples/petstore/pet/PetService.java
@@ -18,27 +18,42 @@
  */
 package org.wso2.carbon.mss.examples.petstore.pet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wso2.carbon.mss.MicroservicesRunner;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 
 /**
  * Pet microservice
  */
+@Path("/pet")
 public class PetService {
+    private static final Logger log = LoggerFactory.getLogger(PetService.class);
 
+
+    @PUT
     public void addPet() {
-
+        log.info("Added pet");
     }
 
+    @DELETE
     public void deletePet() {
-
+        log.info("Deleted pet");
     }
 
+    @POST
     public void updatePet() {
-
+        log.info("Updated pet");
     }
 
+    @GET
     public void getPet() {
-
+        log.info("Got pet");
     }
 
     public static void main(String[] args) {
