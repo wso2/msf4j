b6b17b85b915b57b9f27838a3152e9cae96fba4a
Sagara <sagara.gunathunga@gmail.com>
2015-10-20 20:35:35 +0530

    Added Java Beans menthods

--- samples/petstore/util/src/main/java/org/wso2/carbon/mss/examples/petstore/util/model/Category.java
+++ samples/petstore/util/src/main/java/org/wso2/carbon/mss/examples/petstore/util/model/Category.java
@@ -25,11 +25,17 @@ public class Category {
 
     private String name;
 
+    public Category() {}
+
     public Category(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
