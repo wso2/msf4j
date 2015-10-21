2d3791102e24de3adb5b91d757c4eeba75c1c3d6
Afkham Azeez <azeez@wso2.com>
2015-10-21 18:18:56 +0530

    Show usage if the proper init params are not passed

--- samples/petstore/microservices/pet/etc/init.sh
+++ samples/petstore/microservices/pet/etc/init.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+if [ -z "$1" ] || [ -z "$2" ]; then
+    echo "Usage: init.sh <host> <port>"
+    exit 1
+fi
+
 for i in {0..10}
 do
   age=$((1+$i))
   price=`echo 100.99 + $i | bc`
 
   curl -v -X POST -H "Content-Type:application/json" \
-  -d '{"id":"pet-'"$i"'","category":{"name":"cat"},"ageMonths":"'"$age"'","price":"'"$price"'","dateAdded":1444827363766,"image":"http://foo.com/cat'"$i"'.png"}' \
+  -d '{"category":{"name":"cat"},"ageMonths":"'"$age"'","price":"'"$price"'","image":"http://foo.com/cat'"$i"'.png"}' \
   http://$1:$2/pet/
 done
 
