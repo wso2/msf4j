049152bae090aa33bf603ccd268266214e807e30
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-10-29 21:49:28 +0530

    add pet service constances

--- html/config/config.php
+++ html/config/config.php
@@ -2,4 +2,6 @@
 //get security server env varaible
 $security_server = getenv('');
 define("SECURITY_SERVER", "security");
-define("SECURITY_SERVER_PORT", "80");
+define("SECURITY_SERVER_PORT", "8080");
+define("PET_SERVICE", "192.168.58.10");
+define("PET_SERVICE_PORT", "8080");
