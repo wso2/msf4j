dbd26b419aa00ad658e8fa9407c44425209d4180
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-10-29 21:43:01 +0530

    refactor auth function

--- html/controllers/authentication/auth.php
+++ html/controllers/authentication/auth.php
@@ -6,4 +6,4 @@ $username = htmlspecialchars($_POST["username"]);
 $password = htmlspecialchars($_POST["password"]);
 
 $auth_json = json_encode(array('name' => $username, 'password' => $password));
-callAuthAPI('http://'.SECURITY_SERVER.':'.SECURITY_SERVER_PORT.'/user/login', $auth_json);
+callAuthApiLogin('http://'.SECURITY_SERVER.':'.SECURITY_SERVER_PORT.'/user/login', $auth_json);
