6a4e1c65ef4ff612bce6814535e6081bd9d35b75
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-10-30 02:06:47 +0530

    add basic resouces

---
+++ html/config/config.php
@@ -0,0 +1,10 @@
+<?php
+//get security server env varaible
+$pet_service = getenv('PET_SERVICE_HOST');
+$pet_service_port = getenv('PET_SERVICE_PORT');
+
+define("SECURITY_SERVER", "security");
+define("SECURITY_SERVER_PORT", "80");
+define("PET_SERVICE", $pet_service);
+define("PET_SERVICE_PORT", $pet_service_port);
+define("FILE_SERVER", "192.168.58.8");
