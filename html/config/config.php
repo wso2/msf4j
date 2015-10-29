2966f9c464bfea2e151b9c2a53b6eb9d4355888c
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-10-29 22:51:26 +0530

    read config using env variables

--- html/config/config.php
+++ html/config/config.php
@@ -1,7 +1,9 @@
 <?php
 //get security server env varaible
-$security_server = getenv('');
+$pet_service = getenv('FE_PET_SERVICE_HOST');
+$pet_service_port = getenv('FE_PET_SERVICE_PORT');
+
 define("SECURITY_SERVER", "security");
-define("SECURITY_SERVER_PORT", "8080");
-define("PET_SERVICE", "192.168.58.10");
-define("PET_SERVICE_PORT", "8080");
+define("SECURITY_SERVER_PORT", "80");
+define("PET_SERVICE", $pet_service);
+define("PET_SERVICE_PORT", $pet_service_port);
