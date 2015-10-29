00c34750c7cc4eb290360837c0bd4c72523ed2c9
Dakshika Jayathilaka <sinhaladjs@gmail.com>
2015-10-30 00:07:49 +0530

    update config env

--- html/config/config.php
+++ html/config/config.php
@@ -1,7 +1,7 @@
 <?php
 //get security server env varaible
-$pet_service = getenv('FE_PET_SERVICE_HOST');
-$pet_service_port = getenv('FE_PET_SERVICE_PORT');
+$pet_service = getenv('PET_SERVICE_HOST');
+$pet_service_port = getenv('PET_SERVICE_PORT');
 
 define("SECURITY_SERVER", "security");
 define("SECURITY_SERVER_PORT", "80");
