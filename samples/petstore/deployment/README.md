6cab553fe5236e8606b61cf9c6487bc2546b801e
Lakmal Warusawithana <lakwarus@gmail.com>
2015-10-30 05:40:06 +0530

    added petstore automated deployment scripts

---
+++ samples/petstore/deployment/README.md
@@ -0,0 +1,1 @@
+# wso2mss-automation
---
+++ samples/petstore/deployment/path.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+PRE_REQ=1
+HOME=`pwd`
+VAGRANT_HOME="$HOME/kubernetes-vagrant-coreos-cluster"
+SHARE_FOLDER="$VAGRANT_HOME/docker/"
+PET_HOME="../microservices/pet"
+FILESERVER_HOME="../microservices/fileserver"
+REDIS_HOME="../microservices/redis"
+FRONTEND_ADMIN="../microservices/frontend-admin"
+FRONTEND_USER="../microservices/frontend-user"
+SECURITY="../microservices/security"
+TRANSACTION="../microservices/transaction"
+
