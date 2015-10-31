38a6fa5653ca35071d5eee00c31cb49392d7a36a
Kalpa Welivitigoda <callkalpa@gmail.com>
2015-10-31 12:38:03 +0530

    fixing jar name in the startup script in metrics sample

--- samples/metrics/start-jar.sh
+++ samples/metrics/start-jar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export METRICS_REPORTING_DAS_DATAAGENTCONFIGPATH=data-agent-conf.xml
 export HTTP_MONITORING_DAS_DATAAGENTCONFIGPATH=data-agent-conf.xml
-java -jar target/metrics-interceptor-*.jar 
+java -jar target/metrics-*.jar
 
