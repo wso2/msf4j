50bcdd7bc0db4e2a9d9237c7cc1349f36347c20b
Samiyuru <samiyuru@wso2.com>
2015-11-05 14:53:26 +0530

    Test InputStream content equality for tests

--- carbon-mss/components/org.wso2.carbon.mss/src/test/java/org/wso2/carbon/mss/internal/router/HttpServerTest.java
+++ carbon-mss/components/org.wso2.carbon.mss/src/test/java/org/wso2/carbon/mss/internal/router/HttpServerTest.java
@@ -39,8 +39,10 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.wso2.carbon.mss.MicroservicesRunner;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.RandomAccessFile;
@@ -604,6 +606,25 @@ public class HttpServerTest {
 
     protected void writeContent(HttpURLConnection urlConn, String content) throws IOException {
         urlConn.getOutputStream().write(content.getBytes(Charsets.UTF_8));
+    }
+
+    protected boolean isStreamEqual(InputStream input1, InputStream input2) throws IOException {
+        if (!(input1 instanceof BufferedInputStream)) {
+            input1 = new BufferedInputStream(input1);
+        }
+        if (!(input2 instanceof BufferedInputStream)) {
+            input2 = new BufferedInputStream(input2);
+        }
+        int ch = input1.read();
+        while (-1 != ch) {
+            int ch2 = input2.read();
+            if (ch != ch2) {
+                return false;
+            }
+            ch = input1.read();
+        }
+        int ch2 = input2.read();
+        return (ch2 == -1);
     }
 
     protected Pet petInstance() {
