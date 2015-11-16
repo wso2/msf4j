21dd0c0059e099672b8760971744b9291b41a5f8
Samiyuru <samiyuru@wso2.com>
2015-11-16 12:43:20 +0530

    Send files as chunks

--- carbon-mss/components/org.wso2.carbon.mss/src/main/java/org/wso2/carbon/mss/internal/router/BasicHttpResponder.java
+++ carbon-mss/components/org.wso2.carbon.mss/src/main/java/org/wso2/carbon/mss/internal/router/BasicHttpResponder.java
@@ -20,26 +20,23 @@
 package org.wso2.carbon.mss.internal.router;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Throwables;
 import com.google.common.collect.Multimap;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.DefaultFileRegion;
-import io.netty.channel.FileRegion;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.DefaultHttpResponse;
+import io.netty.handler.codec.http.HttpChunkedInput;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpResponse;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.stream.ChunkedFile;
 import org.wso2.carbon.mss.ChunkResponder;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.channels.FileChannel;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -103,39 +100,26 @@ public class BasicHttpResponder extends AbstractHttpResponder {
 
     @Override
     public void sendFile(File file, String contentType,
-                         @Nullable Multimap<String, String> headers) {
+                         @Nullable Multimap<String, String> headers) throws IOException {
         Preconditions.checkArgument(responded.compareAndSet(false, true), "Response has been already sent");
         HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
 
         setCustomHeaders(response, headers);
         response.headers().set(HttpHeaders.Names.CONTENT_TYPE, contentType);
-        response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+        // response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, file.length());
+
+        response.headers().set(HttpHeaders.Names.TRANSFER_ENCODING, HttpHeaders.Values.CHUNKED);
+        channel.writeAndFlush(response);
+
+        HttpChunkedInput httpChunkWriter = new HttpChunkedInput(new ChunkedFile(file));
+        ChannelFuture sendFileFuture = channel.write(httpChunkWriter);
 
         final boolean responseKeepAlive = setResponseKeepAlive(response);
-
-        // Write the initial line and the header.
-        channel.write(response);
-
-        // Write the content.
-
-        ChannelFuture writeFuture;
-        try {
-            FileChannel fc = new RandomAccessFile(file, "r").getChannel();
-
-            final FileRegion region = new DefaultFileRegion(fc, 0, file.length());
-            writeFuture = channel.write(region);
-            writeFuture.addListener(new ChannelFutureListener() {
-                @Override
-                public void operationComplete(ChannelFuture future) throws Exception {
-//          region.releaseExternalResources(); //TODO: Azeez fix
-                    if (!responseKeepAlive) {
-                        channel.close();
-                    }
-                }
-            });
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
-        }
+        sendFileFuture.addListener(future -> {
+            if (!responseKeepAlive) {
+                channel.close();
+            }
+        });
     }
 
     private void setCustomHeaders(HttpResponse response, @Nullable Multimap<String, String> headers) {
