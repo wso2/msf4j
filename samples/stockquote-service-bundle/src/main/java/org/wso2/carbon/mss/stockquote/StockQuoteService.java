1ce1fd7882b8927cd7b53c5421d3afbc6b090dc3
Afkham Azeez <azeez@wso2.com>
2015-12-15 12:38:47 +0530

    Changed service toString method

--- samples/stockquote-service-bundle/src/main/java/org/wso2/carbon/mss/stockquote/StockQuoteService.java
+++ samples/stockquote-service-bundle/src/main/java/org/wso2/carbon/mss/stockquote/StockQuoteService.java
@@ -105,6 +105,6 @@ public class StockQuoteService implements Microservice {
 
     @Override
     public String toString() {
-        return "StockQuoteService2{}";
+        return "StockQuoteService-OSGi-bundle";
     }
 }
