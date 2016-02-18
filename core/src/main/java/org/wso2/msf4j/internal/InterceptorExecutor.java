8b89ab539beb4471ae0fc2677dc0514eed55befb
Samiyuru <samiyuru@wso2.com>
2016-02-18 20:20:43 +0530

    Adding an interceptor method executor

---
+++ core/src/main/java/org/wso2/msf4j/internal/InterceptorExecutor.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.msf4j.internal;
+
+import org.wso2.carbon.messaging.CarbonMessage;
+import org.wso2.msf4j.Interceptor;
+import org.wso2.msf4j.ServiceMethodInfo;
+import org.wso2.msf4j.internal.router.HttpResourceModel;
+
+import java.util.List;
+
+/**
+ * Execute Interceptors. preCall and postCall
+ */
+public class InterceptorExecutor {
+
+    private CarbonMessage request;
+    private CarbonMessage response;
+    private List<Interceptor> interceptors;
+    private ServiceMethodInfo serviceMethodInfo;
+
+    private InterceptorExecutor(HttpResourceModel httpResourceModel,
+                                CarbonMessage request,
+                                CarbonMessage response,
+                                List<Interceptor> interceptors) {
+        this.request = request;
+        this.response = response;
+        this.interceptors = interceptors;
+        serviceMethodInfo = new ServiceMethodInfo(httpResourceModel.getMethod().getDeclaringClass().getName(),
+                httpResourceModel.getMethod());
+    }
+
+    public static InterceptorExecutor instance(HttpResourceModel httpResourceModel,
+                                        CarbonMessage request,
+                                        CarbonMessage response,
+                                        List<Interceptor> interceptors) {
+        return new InterceptorExecutor(httpResourceModel, request, response, interceptors);
+    }
+
+    /**
+     * Execute preCalls in all interceptors and return true
+     * if and only if all preCalls return true.
+     *
+     * @return true if all preCalls return true
+     */
+    public boolean execPreCalls() {
+        for (Interceptor interceptor : interceptors) {
+            if (!interceptor.preCall(request, response, serviceMethodInfo)) {
+                // Terminate further request processing if preCall returns false.
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Execute postCalls of all interceptors.
+     *
+     * @param status status that was returned to the client
+     */
+    public void execPostCalls(int status) {
+        for (Interceptor interceptor : interceptors) {
+            interceptor.postCall(request, status, serviceMethodInfo);
+        }
+    }
+}
