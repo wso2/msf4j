ee2c68dede92733abb735beef4084e365f147de8
Samiyuru <samiyuru@wso2.com>
2016-03-03 14:39:25 +0530

    Introducing entity writers to write an entity to a carbon message

---
+++ core/src/main/java/org/wso2/msf4j/entitywriter/EntityWriter.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.msf4j.entitywriter;
+
+import org.wso2.carbon.messaging.CarbonMessage;
+
+/**
+ * Interface that provides the capability of writing an entity type to
+ * a carbon message.
+ *
+ * @param <T> type of the entity
+ */
+public interface EntityWriter<T> {
+
+    String CHUNKED = "chunked";
+
+    /**
+     * Provide supported entity type.
+     *
+     * @return entity type
+     */
+    Class<T> getType();
+
+    /**
+     * Write the entity object to the carbon message by considering the
+     * provided chunk size and media type.
+     *
+     * @param entity    object
+     * @param mediaType user defined media type
+     * @param chunkSize user defined chunk size
+     *                  0 to signify none chunked response
+     *                  -1 to signify default chunk size of the EntityWriter
+     * @throws Exception
+     */
+    void writeData(CarbonMessage carbonMessage, T entity, String mediaType, int chunkSize) throws Exception;
+
+}
---
+++ core/src/main/java/org/wso2/msf4j/entitywriter/ObjectEntityWriter.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.msf4j.entitywriter;
+
+import org.omg.CORBA.Object;
+import org.wso2.carbon.messaging.CarbonMessage;
+import org.wso2.carbon.messaging.Constants;
+import org.wso2.msf4j.Response;
+import org.wso2.msf4j.beanconversion.BeanConverter;
+
+import java.nio.ByteBuffer;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * EntityWriter for  entity of type Object.
+ */
+public class ObjectEntityWriter implements EntityWriter<Object> {
+
+    /**
+     * Supported entity type.
+     */
+    @Override
+    public Class<Object> getType() {
+        return Object.class;
+    }
+
+    /**
+     * Write the entity to the carbon message.
+     */
+    @Override
+    public void writeData(CarbonMessage carbonMessage, Object entity, String mediaType, int chunkSize)
+            throws Exception {
+        mediaType = (mediaType != null) ? mediaType : MediaType.WILDCARD;
+        ByteBuffer byteBuffer = BeanConverter.getInstance().getConverter(mediaType).convertToMedia(entity);
+        carbonMessage.addMessageBody(byteBuffer);
+        carbonMessage.setEndOfMsgAdded(true);
+        if (chunkSize == Response.NO_CHUNK) {
+            carbonMessage.setHeader(Constants.HTTP_TRANSFER_ENCODING, CHUNKED);
+        } else {
+            carbonMessage.setHeader(Constants.HTTP_CONTENT_LENGTH, String.valueOf(byteBuffer.remaining()));
+        }
+        carbonMessage.setHeader(Constants.HTTP_CONTENT_TYPE, mediaType);
+    }
+}
