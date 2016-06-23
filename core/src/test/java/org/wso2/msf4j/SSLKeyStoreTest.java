b133577c2dc87bf628c49a6bb58671c976f4bdb9
Afkham Azeez <azeez@wso2.com>
2016-06-23 10:49:17 +0530

    Properly structured the test related classes

---
+++ core/src/test/java/org/wso2/msf4j/SSLKeyStoreTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.msf4j;
+
+import com.google.common.io.ByteStreams;
+import com.google.common.io.FileWriteMode;
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.msf4j.conf.SSLConfig;
+import org.wso2.msf4j.conf.SSLHandlerFactory;
+
+import java.io.File;
+
+/**
+ * Tests SSL KeyStore behaviour.
+ */
+public class SSLKeyStoreTest {
+
+    public static File tmpFolder;
+    private static File keyStore;
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        keyStore = new File(tmpFolder, "KeyStore.jks");
+        keyStore.createNewFile();
+        ByteStreams.copy(
+                Resources.asByteSource(Resources.getResource("cert.jks")).openStream(),
+                Files.asByteSink(keyStore, FileWriteMode.APPEND).openStream());
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        keyStore.delete();
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSslCertPathConfiguration1() throws IllegalArgumentException {
+        //Bad Certificate Path
+        new SSLHandlerFactory(SSLConfig.builder(new File("badCertificate"), "secret").setCertificatePassword("secret")
+                .build());
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSslCertPathConfiguration2() throws IllegalArgumentException {
+        //Null Certificate Path
+        new SSLHandlerFactory(SSLConfig.builder(null, "secret").setCertificatePassword("secret").build());
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testSslKeyStorePassConfiguration2() throws IllegalArgumentException {
+        //Missing Key Pass
+        new SSLHandlerFactory(SSLConfig.builder(keyStore, null).setCertificatePassword("secret").build());
+    }
+
+    @Test
+    public void testSslCertPassConfiguration() throws IllegalArgumentException {
+        //Bad Cert Pass
+        new SSLHandlerFactory(SSLConfig.builder(keyStore, "secret").build());
+    }
+}
