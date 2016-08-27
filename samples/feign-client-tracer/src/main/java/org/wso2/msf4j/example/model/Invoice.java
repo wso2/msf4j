604209576021470399d9fbe091fc765ff965893b
Akila Perera <ravihansa3000@gmail.com>
2016-08-27 23:58:10 +0530

    Introducing OpenFeign client with tracing support

---
+++ samples/feign-client-tracer/src/main/java/org/wso2/msf4j/example/model/Invoice.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.msf4j.example.model;
+
+import org.wso2.msf4j.example.ModelUtils;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+/**
+ * Represents a stock item. @XmlRootElement is used to support xml conversion.
+ */
+@SuppressWarnings("unused")
+@XmlRootElement
+public class Invoice {
+
+    private String id;
+    private String customerId;
+    private double amount;
+    private Date date;
+
+    /**
+     * No arg constructor is required for xml marshalling
+     */
+    public Invoice() {
+    }
+
+    public Invoice(String id, String customerId, double amount, Date date) {
+        this.id = id;
+        this.customerId = customerId;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return ModelUtils.toString(this);
+    }
+}
