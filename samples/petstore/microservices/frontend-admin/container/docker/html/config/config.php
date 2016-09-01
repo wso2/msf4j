9d693bc3db2435dd4106db689061630dbcd8fa16
Afkham Azeez <azeez@wso2.com>
2016-09-01 16:03:54 +0530

    Properly fix security service connection details

--- samples/petstore/microservices/frontend-admin/container/docker/html/config/config.php
+++ samples/petstore/microservices/frontend-admin/container/docker/html/config/config.php
@@ -4,10 +4,15 @@ $pet_service = getenv('PET_SERVICE_HOST');
 $pet_service_port = getenv('PET_SERVICE_PORT');
 $pet_file_server_port = getenv('FE_FILE_SERVICE_NODE_PORT');
 $pet_file_server = getenv('FE_FILE_SERVICE_HOST');
+$security_service_host = getenv('SECURITY_SERVICE_HOST');
+$security_service_port = getenv('SECURITY_SERVICE_PORT');
 
-define("SECURITY_SERVER", "security");
-define("SECURITY_SERVER_PORT", "80");
+define("SECURITY_SERVER", $security_service_host);
+define("SECURITY_SERVER_PORT", $security_service_port);
 define("PET_SERVICE", $pet_service);
 define("PET_SERVICE_PORT", $pet_service_port);
 define("FILE_SERVER", $pet_file_server);
 define("FILE_SERVER_HOST", $pet_file_server_port);
+
+?>
+
