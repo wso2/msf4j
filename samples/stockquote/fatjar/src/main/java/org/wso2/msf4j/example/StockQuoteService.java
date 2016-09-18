d31542707e764d6694eb19b84d9e3aec90a27f05
Janak Amarasena <janakamarasena@gmail.com>
2016-09-18 12:09:04 +0530

    fixed possible checkstyle issue

--- samples/stockquote/fatjar/src/main/java/org/wso2/msf4j/example/StockQuoteService.java
+++ samples/stockquote/fatjar/src/main/java/org/wso2/msf4j/example/StockQuoteService.java
@@ -30,7 +30,15 @@ import org.wso2.msf4j.example.exception.SymbolNotFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.CookieParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
