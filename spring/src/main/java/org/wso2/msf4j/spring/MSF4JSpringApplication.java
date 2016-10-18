e0bfa3ce2b00eb926d134e5c2a1129b45ca186c0
Sagara <sagara.gunathunga@gmail.com>
2016-10-18 12:14:25 +0530

    Supported for custom ApplicationContextInitializers

--- spring/src/main/java/org/wso2/msf4j/spring/MSF4JSpringApplication.java
+++ spring/src/main/java/org/wso2/msf4j/spring/MSF4JSpringApplication.java
@@ -22,10 +22,23 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.core.GenericTypeResolver;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
 import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
+import org.wso2.msf4j.spring.property.YamlFileApplicationContextInitializer;
 
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
+
 
 /**
  * This class is responsible to construct Microservice application.
@@ -41,7 +54,7 @@ public class MSF4JSpringApplication {
     private Class<?> configurationClass;
     private Class<? extends ConfigurableApplicationContext> applicationContextClass;
     private ResourceLoader resourceLoader;
-    private List<ApplicationContextInitializer<?>> initializers;
+    private List<ApplicationContextInitializer<?>> initializers = new ArrayList<>();
 
     public MSF4JSpringApplication(Class sources) {
         initialize(sources);
@@ -51,13 +64,72 @@ public class MSF4JSpringApplication {
         if (source != null) {
             this.source = source;
         }
+        setInitializers((Collection) getSpringFactoriesInstances(ApplicationContextInitializer.class));
+        addInitializers(new YamlFileApplicationContextInitializer());
+    }
+
+
+    public void setInitializers(Collection<? extends ApplicationContextInitializer<?>> initializers) {
+        this.initializers = new ArrayList<>();
+        this.initializers.addAll(initializers);
+    }
+
+    private <T> Collection<? extends T> getSpringFactoriesInstances(Class<T> type) {
+        return getSpringFactoriesInstances(type, new Class<?>[]{});
+    }
+
+    private <T> Collection<? extends T> getSpringFactoriesInstances(Class<T> type,
+                                                                    Class<?>[] parameterTypes, Object... args) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        Set<String> names = new LinkedHashSet<>(
+                SpringFactoriesLoader.loadFactoryNames(type, classLoader));
+        List<T> instances = createSpringFactoriesInstances(type, parameterTypes,
+                classLoader, args, names);
+        AnnotationAwareOrderComparator.sort(instances);
+        return instances;
+    }
+
+    private <T> List<T> createSpringFactoriesInstances(Class<T> type,
+                                                       Class<?>[] parameterTypes, ClassLoader classLoader,
+                                                       Object[] args, Set<String> names) {
+        List<T> instances = new ArrayList<T>(names.size());
+        for (String name : names) {
+            try {
+                Class<?> instanceClass = ClassUtils.forName(name, classLoader);
+                Assert.isAssignable(type, instanceClass);
+                Constructor<?> constructor = instanceClass
+                        .getDeclaredConstructor(parameterTypes);
+                T instance = (T) BeanUtils.instantiateClass(constructor, args);
+                instances.add(instance);
+            } catch (Throwable ex) {
+                throw new IllegalArgumentException(
+                        "Cannot instantiate " + type + " : " + name, ex);
+            }
+        }
+        return instances;
+    }
+
+    public void addInitializers(ApplicationContextInitializer<?>... initializers) {
+        this.initializers.addAll(Arrays.asList(initializers));
     }
 
     public static ConfigurableApplicationContext run(Class sources, String... args) {
-        return new MSF4JSpringApplication(sources).run(args);
+        MSF4JSpringApplication application = new MSF4JSpringApplication(sources);
+        ConfigurableApplicationContext context = application.run(args);
+        application.applyInitializers(context);
+        return context;
     }
 
-    public ConfigurableApplicationContext run(String... args) {
+    private void applyInitializers(ConfigurableApplicationContext context) {
+        for (ApplicationContextInitializer initializer : getInitializers()) {
+            Class<?> requiredType = GenericTypeResolver.resolveTypeArgument(
+                    initializer.getClass(), ApplicationContextInitializer.class);
+            Assert.isInstanceOf(requiredType, context, "Unable to call initializer.");
+            initializer.initialize(context);
+        }
+    }
+
+    protected ConfigurableApplicationContext run(String... args) {
         ConfigurableApplicationContext context = createApplicationContext();
         if (configurationClass != null) {
             registerIfAnnotationConfigApplicationContext(context);
