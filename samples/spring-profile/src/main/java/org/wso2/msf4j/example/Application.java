ca7e93c1fd6413f8307689f8b41c04bb1309434d
Thusitha Dayaratne <thusithad@wso2.com>
2016-10-31 19:30:41 +0530

    Adding application.yml based configuration support to msf4j-spring

---
+++ samples/spring-profile/src/main/java/org/wso2/msf4j/example/Application.java
@@ -0,0 +1,13 @@
+package org.wso2.msf4j.example;
+
+import org.wso2.msf4j.spring.MSF4JSpringApplication;
+
+/**
+ * Main Application class to run this sample.
+ */
+public class Application {
+
+	public static void main(String[] args) {
+        MSF4JSpringApplication.run(Application.class, args);
+	}
+}
